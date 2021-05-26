6a38a595abf164e3b02c0a4f7ea9e31be8becae4
rdenden <dddrrr814@gmail.com>
2021-05-27 01:45:38 +0900

    1_3

---
+++ 1_3equalsオーバーライド雛形/Hero.java
@@ -0,0 +1,20 @@
+public class Hero {
+    public String name;
+    
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Hero)) {
+            return false;
+        }
+        Hero r = (Hero) o;
+        if (!this.name.trim().equals(r.name.trim())) {
+            return false;
+        }
+        return true;
+    }
+}
