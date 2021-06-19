d0460083d48ca80df3aacd2b085b2c79917c0729
rdenden <dddrrr814@gmail.com>
2021-06-20 03:15:05 +0900

    列挙型

---
+++ 2_8列挙型/Account .java
@@ -0,0 +1,12 @@
+public class Account {
+    private String accountNo;
+    private int zandaka;
+    private AccountType accountType;
+    
+    public Account(String aNo,AccountType aType) {
+        
+    }
+    public static final int FUTSU = 1;
+    public static final int TOUZA = 2;
+    public static final int TEIKI = 3;
+}
